debae6fde14a8e0c4632ba0bf01bac9f0e3cc372
HENG_Billy <144659596+HENGBilly@users.noreply.github.com>
2024-11-20 14:28:24 +0000

    ajout de tout le js et css pour le formulaire qui verif le tout pour mettre en rouge/vert

--- Formulaire/assets/js/myscript.js
+++ Formulaire/assets/js/myscript.js
@@ -5,6 +5,7 @@ pswd = document.querySelector('[name="password"]');
 verif = document.querySelectorAll('small');
 msg = document.querySelector('[name="message"]');
 age = document.getElementById('formCheck-1');
+age2 = document.querySelector('label');
 
 function isValidEmail(email) {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -14,52 +15,48 @@ function isValidEmail(email) {
 function verifierFormulaire() {
     console.log("test");
     if (pre.value === '') {
-        pre.classList.add('valide');
-        pre.classList.remove('pas-valide');
-        console.log("test1");
-    } else {
         pre.classList.remove('valide');
         pre.classList.add('pas-valide');
-        console.log("test2");
+    } else {
+        pre.classList.add('valide');
+        pre.classList.remove('pas-valide');
     }
     if (nom.value === '') {
-        nom.classList.add('valide');
-        nom.classList.remove('pas-valide');
-        console.log("test3");
-    } else {
         nom.classList.remove('valide');
         nom.classList.add('pas-valide');
-        console.log("test4");
+    } else {
+        nom.classList.add('valide');
+        nom.classList.remove('pas-valide');
     }
-    if (isValidEmail(email)) {
-        console.log("test5");
+    if (isValidEmail(email.value)) {
         email.classList.add('valide');
         email.classList.remove('pas-valide');
     } else {
-        console.log("test6");
         email.classList.remove('valide');
         email.classList.add('pas-valide');
     }
-    if (pswd.length >= 8){
-        console.log("test7");
+    if (pswd.value.length >= 8){
         pswd.classList.add('valide');
         pswd.classList.remove('pas-valide');
         verif[0].classList.add('invisible');
     } else {
-        console.log("test8");
         pswd.classList.remove('valide');
         pswd.classList.add('pas-valide');
         verif[0].classList.remove('invisible');
     }
-    if (age.checked) {
-        console.log("test9");
-        age.classList.add('valide');
-        age.classList.remove('pas-valide');
+    if(msg.value === ''){
+        msg.classList.remove('valide');
+        msg.classList.add('pas-valide');
     } else {
-        console.log("test10");
-        age.classList.remove('valide');
-        age.classList.add('pas-valide');
+        msg.classList.add('valide');
+        msg.classList.remove('pas-valide');
     }
-
+    if (age.checked) {
+        age2.classList.add('tvalide');
+        age2.classList.remove('tpas-valide');
+    } else {
+        age2.classList.remove('tvalide');
+        age2.classList.add('tpas-valide');
+    }
 
 }
